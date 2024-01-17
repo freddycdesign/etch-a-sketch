64b204e05d98f814f663cc66e2f2d60ff129cc27
Freddy Chen <77744025+freddycdesign@users.noreply.github.com>
2024-01-17 11:39:37 -0800

    Added user prompt for number of blocks per side Javascript update to prompt user input for number of block per side and adjust the width & height of new blocks accordingly

--- script-main.js
+++ script-main.js
@@ -4,16 +4,23 @@ const gridContainer = document.getElementById("gridContainer");
 
 //gridBox.style.backgroundColor = "black";
 //gridBox.style.width = "200px";
+alert("Enter number of blocks per side");
+const userInput = prompt()
+const numOfBlocks = userInput
+const newWidth = 600 / numOfBlocks
 
 function addBoxes(row) {
     for (let i = 0; i < row * row; i++) {
         const gridBox = document.createElement("div");
         gridContainer.appendChild(gridBox);
         gridBox.classList.add("gridBox");
-        //gridBox.style.width = 150 + "px";
+        gridBox.style.width = newWidth + "px";
     }
 }
 
-addBoxes(3);
+//gridBox.style.width = newWidth + "px";
 
+
+addBoxes(numOfBlocks);
+console.log(newWidth);
 console.log(gridContainer.clientWidth);
