627cf1531d7da77d1c5027b30b2ab0d630a7fcee
Freddy Chen <77744025+freddycdesign@users.noreply.github.com>
2024-01-17 11:10:18 -0800

    Fixed container with red blocks Made a fixed container with red javascript generated div blocks to fill

--- script-main.js
+++ script-main.js
@@ -1,1 +1,19 @@
 console.log("link test");
+
+const gridContainer = document.getElementById("gridContainer");
+
+//gridBox.style.backgroundColor = "black";
+//gridBox.style.width = "200px";
+
+function addBoxes(row) {
+    for (let i = 0; i < row * row; i++) {
+        const gridBox = document.createElement("div");
+        gridContainer.appendChild(gridBox);
+        gridBox.classList.add("gridBox");
+        //gridBox.style.width = 150 + "px";
+    }
+}
+
+addBoxes(3);
+
+console.log(gridContainer.clientWidth);
